3d4f872d7a4afe3b9273d6a86e0a37f308bc6c06
Mohammad kh <Mohammad10.kh@gmail.com>
2020-04-24 20:25:05 +0430

    init

---
+++ app/src/main/java/com/mamad/calculator/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.mamad.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun clickButtonEvent(view: View){
+        Toast.makeText(this, "a button clicked",Toast.LENGTH_SHORT).show()
+    }
+
+}
